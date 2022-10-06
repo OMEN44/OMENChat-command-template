4fd3944562d3875deeec0a6d80e80afadb3073f2
OMEN44 <huonswales@gmail.com>
2022-10-06 10:06:54 +1000

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import com.github.OMEN44.OmenChatPlugin;
+
+public class Main implements OmenChatPlugin {
+    @Override
+    public void onLoad() {
+        setCommandExecutor("testCommand", new TestCommand());
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
--- src/main/java/TestCommand.java
+++ src/main/java/TestCommand.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Command {
+import com.github.OMEN44.Command;
+
+public class TestCommand implements Command {
+
+    @Override
+    public boolean execute(String label, String sender, String timeSent, String[] args) {
+        System.out.println("Plugin works!");
+        return true;
+    }
 }
